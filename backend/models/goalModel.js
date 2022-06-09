3ca99225be9d132e57e76db96ed4f2d59d2d2522
Jikjii <85137475+Jikjii@users.noreply.github.com>
2022-06-09 13:50:17 -0400

    crud functionality working w/ postman

---
+++ backend/models/goalModel.js
@@ -0,0 +1,16 @@
+// creating schema
+const mongoose = require("mongoose");
+
+const goalSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, "Please add a text value"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Goal", goalSchema);
