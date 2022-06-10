c70a921ac9a34db434167e4a559296d514777fff
Jikjii <85137475+Jikjii@users.noreply.github.com>
2022-06-10 11:11:44 -0400

    goal and user updated

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,15 @@
 const asyncHandler = require("express-async-handler");
 
 const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 // @desc    Get goals
 // @route   GET /api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  // Updated - will now only get specific users goals => this is because
+  // I added the "protect" middleware to the routes
+  const goals = await Goal.find({ user: req.user.id });
 
   res.status(200).json(goals);
 });
@@ -22,6 +25,7 @@ const setGoal = asyncHandler(async (req, res) => {
 
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
 
   res.status(200).json(goal);
@@ -38,26 +42,53 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    // Check for user
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
-  })
+  });
 
-  res.status(200).json(updatedGoal)
+  res.status(200).json(updatedGoal);
 });
 
 // @desc    Delete goal
 // @route   GET /api/goals/:id
 //@access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    const goal = await Goal.findById(req.params.id);
+  const goal = await Goal.findById(req.params.id);
 
-    if(!goal) {
-        res.status(400)
-        throw new Error("Goal not found")
-    }
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
 
-    await goal.remove()
+  const user = await User.findById(req.user.id);
 
+  if (!user) {
+    // Check for user
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  await goal.remove();
 
   res.status(200).json({ id: req.params.id });
 });
